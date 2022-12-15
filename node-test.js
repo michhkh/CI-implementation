9845d88ed9dabfe6952a94eba19e4ab36dbe1a03
michhkh <michaelh.kodehode@gmail.com>
2022-12-15 13:32:27 +0100

    revert lint fix

--- node-test.js
+++ node-test.js
@@ -1,5 +1,6 @@
-console.log('This node test is working as intended and fresh and deleted');
+console.log("This node test is working as intended and fresh and deleted");
 
-const attack = (creatureName, damage, isCritical) => `${creatureName} dealt ${isCritical ? damage * 2 : damage} damage!`;
+const attack = (creatureName, damage, isCritical) =>
+  `${creatureName} dealt ${isCritical ? damage * 2 : damage} damage!`;
 
-console.log(attack('Kylo Ren', 1));
+console.log(attack("Kylo Ren", 1));
